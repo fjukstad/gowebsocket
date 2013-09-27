7e197148b88cab4129b0e362cb863c252e242f46
Vegard Sandengen <vegardsandengen@gmail.com>
2013-09-27 11:10:25 +0200

    Ran formatter, fixed the client side bug i introduced and altered test to reflect changes

--- gowebsocket.go
+++ gowebsocket.go
@@ -1,150 +1,138 @@
-
 // Implementation inspired by http://gary.beagledreams.com/page/go-websocket-chat.html
 
 package gowebsocket
 
 import (
-    "code.google.com/p/go.net/websocket"
-    "log"
-    "net/http"
+	"code.google.com/p/go.net/websocket"
+	"log"
+	"net/http"
 )
 
 type hub struct {
-    
-    // registered cnnections
-    connections map[*connection] bool
 
-    // inbound messages from connections
-    broadcast chan string
+	// registered cnnections
+	connections map[*connection]bool
 
-    // register requests from connections
-    register chan *connection
+	// inbound messages from connections
+	broadcast chan string
 
-    // unregister requests from connections
-    unregister chan *connection
-    
+	// register requests from connections
+	register chan *connection
+
+	// unregister requests from connections
+	unregister chan *connection
 }
 
-var h = hub {
-    connections:    make(map[*connection] bool),
-    broadcast:      make(chan string),
-    register:       make(chan *connection),
-    unregister:     make(chan *connection),
+var h = hub{
+	connections: make(map[*connection]bool),
+	broadcast:   make(chan string),
+	register:    make(chan *connection),
+	unregister:  make(chan *connection),
 }
 
+func (h *hub) Run() {
+	for {
+		select {
 
+		// Register new connection
+		case c := <-h.register:
+			h.connections[c] = true
 
+		// Unregister connection
+		case c := <-h.unregister:
+			delete(h.connections, c)
+			close(c.send)
 
-func (h *hub) Run(){
-    for {
-        select {
-            
-            // Register new connection
-            case c:= <-h.register:
-                h.connections[c] = true
+		// Broadcast message to all connections. If send buffer
+		// is full unregister and close websocket connection
+		case m := <-h.broadcast:
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+				default:
+					delete(h.connections, c)
+					close(c.send)
+					go c.conn.Close()
+				}
+			}
 
-            // Unregister connection
-            case c:= <-h.unregister:
-                delete(h.connections, c)
-                close(c.send)
-
-            // Broadcast message to all connections. If send buffer
-            // is full unregister and close websocket connection 
-            case m:= <-h.broadcast:
-                for c:= range h.connections {
-                    select {
-                        case c.send <- m:
-                        default:
-                            delete(h.connections, c)
-                            close(c.send)
-                            go c.conn.Close() 
-                    }
-                }
-                
-        }
-    }
+		}
+	}
 }
-
 
 type connection struct {
-    // connection
-    conn *websocket.Conn
-    
-    // buffered channel of outbund messages
-    send chan string
+	// connection
+	conn *websocket.Conn
 
+	// buffered channel of outbund messages
+	send chan string
 }
 
 func (c *connection) reader(h *hub) {
-    for {
-        var message [1000]byte
-        n, err := c.conn.Read(message[:])
-        if err != nil{
-            break
-        }
-        h.broadcast <- string(message[:n])
-    }
-    c.conn.Close()
+	for {
+		var message [1000]byte
+		n, err := c.conn.Read(message[:])
+		if err != nil {
+			break
+		}
+		h.broadcast <- string(message[:n])
+	}
+	c.conn.Close()
 }
-
 
 func (c *connection) writer(h *hub) {
-    for message := range c.send{
-        err := websocket.Message.Send(c.conn, message)
-        if err != nil {
-            break
-        }
-    }
+	for message := range c.send {
+		err := websocket.Message.Send(c.conn, message)
+		if err != nil {
+			break
+		}
+	}
 }
 
-func (s *WSServer) connHandler(conn *websocket.Conn){
-    s.Conn = &connection{send: make(chan string, 256), conn: conn}
-    s.Hub.register <- s.Conn
-    defer func() {
-        s.Hub.unregister <- s.Conn
-    }() 
-    
-    go s.Conn.writer(s.Hub)
-    s.Conn.reader(s.Hub)
-}
+func (s *WSServer) connHandler(conn *websocket.Conn) {
+	s.Conn = &connection{send: make(chan string, 256), conn: conn}
+	s.Hub.register <- s.Conn
+	defer func() {
+		s.Hub.unregister <- s.Conn
+	}()
 
+	go s.Conn.writer(s.Hub)
+	s.Conn.reader(s.Hub)
+}
 
 type WSServer struct {
-    Hub *hub
-    Server *http.Server
-    Conn *connection
+	Hub    *hub
+	Server *http.Server
+	Conn   *connection
 }
 
-func New (ip, port string) (s *WSServer) {
-    s = new(WSServer) 
+func New(ip, port string) (s *WSServer) {
+	s = new(WSServer)
 
-    s.Hub  =  &hub {
-        connections:    make(map[*connection] bool),
-        broadcast:      make(chan string),
-        register:       make(chan *connection),
-        unregister:     make(chan *connection),
-    }
+	s.Hub = &hub{
+		connections: make(map[*connection]bool),
+		broadcast:   make(chan string),
+		register:    make(chan *connection),
+		unregister:  make(chan *connection),
+	}
 
+	s.Server = &http.Server{
+		Addr:    ip + port,
+		Handler: websocket.Handler(s.connHandler),
+	}
 
-    s.Server = &http.Server{
-        Addr: ip+port,
-        Handler: websocket.Handler(s.connHandler),
-    }
-    
-    return s
+	return s
 }
 
 func (s *WSServer) Start() {
-    go s.Hub.Run() 
+	go s.Hub.Run()
 
-    go func () {
-        http.Handle("/ws", s.Server.Handler)
-        err := s.Server.ListenAndServe();
-        if err != nil {
-            log.Panic("Websocket server could not start") 
-        }
-    }() 
-    log.Print("Websocket server started successfully. Go have fun! ") 
+	go func() {
+		http.Handle("/ws", s.Server.Handler)
+		err := s.Server.ListenAndServe()
+		if err != nil {
+			log.Panic("Websocket server could not start")
+		}
+	}()
+	log.Print("Websocket server started successfully. Go have fun! ")
 }
-
-
--- test/websocket-test.go
+++ test/websocket-test.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "github.com/fjukstad/gowebsocket"
-    "flag"
-    "log"
-    "time"
-    "strconv"
+	"flag"
+	"github.com/fjukstad/gowebsocket"
+	"log"
+	"strconv"
+	"time"
 )
 
 // Usage example
 func main() {
-    
-    // cmd line flags
-    var ip = flag.String("ip", "127.0.0.1", "ip to run on")
-    var port = flag.String("port", ":3999" ,"port to run on")
-    flag.Parse() 
-    
-    // Start Server
-    server := gowebsocket.New(*ip, *port)
-    server.Start() 
 
+	// cmd line flags
+	var ip = flag.String("ip", "127.0.0.1", "ip to run on")
+	var port = flag.String("port", ":3999", "port to run on")
+	flag.Parse()
 
-    // Start a single client which sends and receives messages 
-    c := gowebsocket.NewClient(*ip, *port) 
-    i := 0
-    for {
-        time.Sleep(1 * time.Second) 
-        
-        msg := "Hello, Websocket. This is message "+strconv.Itoa(i)
+	// Start Server
+	server := gowebsocket.New(*ip, *port)
+	server.Start()
 
-        log.Printf("Sending: %s \n", msg)
+	// Start a single client which sends and receives messages
+	c, err := gowebsocket.NewClient(*ip, *port)
+	if err != nil {
+		log.Panic("Unable to start WS Client with err: ", err)
+	}
+	i := 0
+	for {
+		time.Sleep(1 * time.Second)
 
-        c.Send(msg) 
-        recv := c.Receive() 
+		msg := "Hello, Websocket. This is message " + strconv.Itoa(i)
 
-        log.Printf("Received: %s\n", recv)
-        i++
-    }
+		log.Printf("Sending: %s \n", msg)
 
+		c.Send(msg)
+		recv := c.Receive()
 
-    
+		log.Printf("Received: %s\n", recv)
+		i++
+	}
+
 }
